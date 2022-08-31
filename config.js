49a9d3b5b890237e715eafeef842098aea6f7817
louk123 <106319242+louk123@users.noreply.github.com>
2022-08-31 19:34:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['447451239331','Arctix',true],
-  ['447451231393','Arceus',true],
-  ['919337956084', 'sibu', true],
+  ['212609493719','Arctix',true],
+  ['212609493719','Arceus',true],
+  ['212609493719', 'sibu', true],
   //['447451240296']
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
